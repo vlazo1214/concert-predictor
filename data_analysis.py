46d544dc927edc94fa1622758dd3bd00b417abb7
vlazo1214 <vincentl1214@yahoo.com>
2024-07-14 11:33:54 -0400

    added main and data analysis boilerplate

---
+++ data_analysis.py
@@ -0,0 +1,7 @@
+# script to analyze concerts.csv
+
+
+# read in concerts.csv
+
+
+# iterate through and find the venue + find a way to convert to long/lat
---
+++ main.py
@@ -0,0 +1,9 @@
+# script to run all scripts
+# maybe this will be gui script?
+
+from scraper import getWriteShows
+
+
+if __name__ == "__main__":
+    # getWriteShows(input)
+    getWriteShows()
